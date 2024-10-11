2f0143d44469cd0d0144d5746e282904fadb4318
Jose Tovar <jtovar26455462@gmail.com>
2024-10-11 15:48:04 -0400

    fix: correct change when status is similar from old

--- src/commands/utility/changeStatus.js
+++ src/commands/utility/changeStatus.js
@@ -36,18 +36,19 @@ module.exports = {
       if (!channel.isThread()) return await interaction.reply({ content: "Sorry, this is not a thread!", ephemeral: true });
 
       const status = options.getString('status');
+      const channelName = channel.name;
       const newStatus = MAPPED_STATUS_COMMANDS[status];
 
       if (!newStatus) {
         return await interaction.reply("Invalid status command.");
       }
 
-      const oldStatus = COMMAND_KEYS.find(command => channel.name.startsWith(MAPPED_STATUS_COMMANDS[command]));
+      const oldStatus = COMMAND_KEYS.find(command => channelName.split(' ')[0] === MAPPED_STATUS_COMMANDS[command]);
       const regex = new RegExp(`(${MAPPED_STATUS_COMMANDS[oldStatus]})`, 'g');
-      const channelNameWithStatus = oldStatus ? channel.name.replace(regex, newStatus) : `${newStatus} ${channel.name}`;
+      const channelNameWithStatus = oldStatus ? channelName.replace(regex, newStatus) : `${newStatus} ${channelName}`;
 
       await channel.setName(channelNameWithStatus);
-      await interaction.reply(`Status updated to ${channelNameWithStatus}`);
+      await interaction.reply(`Status updated to ${status}`);
     } catch (error) {
       await interaction.reply("An error occurred while updating the status.");
     }

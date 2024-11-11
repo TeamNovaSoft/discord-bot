780c013bbb8d0403b298ff0adf9c360f5428f429
Alberto Rey <ajrey1282@gmail.com>
2024-11-11 16:20:36 -0500

    feat: Add command to get query for search my points

---
+++ src/commands/utility/search-my-points.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("my-points-query")
+    .setDescription(
+      "Get the Discord search query to check points for a specific month"
+    )
+
+    .addIntegerOption((option) =>
+      option.setName("year").setDescription("Year to search").setRequired(false)
+    )
+
+    .addIntegerOption((option) =>
+      option
+        .setName("month")
+        .setDescription("Month to search")
+        .setRequired(false)
+    )
+
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User to search for")
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const currentDate = new Date();
+    const year =
+      interaction.options.getInteger("year") || currentDate.getFullYear();
+    const month =
+      interaction.options.getInteger("month") || currentDate.getMonth() || 12;
+    const user = interaction.options.getUser("user") || interaction.user;
+
+    const taskCompletedTagId = process.env.TASK_COMPLETED_TAG_ID;
+    const addPointTagId = process.env.ADD_POINT_TAG_ID;
+    const boostedPointTagId = process.env.ADD_BOOSTED_POINT_TAG_ID;
+
+    const targetStartDate = new Date(year, month - 1, 1);
+    const targetEndDate = new Date(year, month, 1);
+
+    const startDateStr = `${targetStartDate.getFullYear()}-${String(
+      targetStartDate.getMonth() + 1
+    ).padStart(2, "0")}-01`;
+    const endDateStr = `${targetEndDate.getFullYear()}-${String(
+      targetEndDate.getMonth() + 1
+    ).padStart(2, "0")}-01`;
+
+    const escapedUserId = `<@${user.id}>`;
+
+    const taskCompletedQuery = `before: ${endDateStr} after: ${startDateStr} <@&${taskCompletedTagId}> ${escapedUserId}`;
+    const addPointQuery = `before: ${endDateStr} after: ${startDateStr} <@&${addPointTagId}> ${escapedUserId}`;
+    const boostedPointQuery = `before: ${endDateStr} after: ${startDateStr} <@&${boostedPointTagId}> ${escapedUserId}`;
+
+    await interaction.reply(
+      `Here are your search queries:\n\n**Tasks completed:**\n\`\`\`${taskCompletedQuery}\`\`\`\n\n**Points obtained:**\n\`\`\`${addPointQuery}\`\`\`\n\n**Boosted Points obtained:**\n\`\`\`${boostedPointQuery}\`\`\``
+    );
+  },
+};

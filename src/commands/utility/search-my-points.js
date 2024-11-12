749d04ca0acaae7e1610a21c25a6906ed4080716
Alberto Rey <ajrey1282@gmail.com>
2024-11-11 20:55:20 -0500

    fix: delete comment

--- src/commands/utility/search-my-points.js
+++ src/commands/utility/search-my-points.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config(); 
 
 module.exports = {
   data: new SlashCommandBuilder()

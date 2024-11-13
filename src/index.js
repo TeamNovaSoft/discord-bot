5ef62f8365c7c28ef74680871e79e776a2a94de5
Jose Tovar <jtovar26455462@gmail.com>
2024-11-13 18:14:18 -0400

    feat: add intentBit for messageCreate event

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 require("./deploy-commands");
 const { DISCORD_CONFIG } = require('./config');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");

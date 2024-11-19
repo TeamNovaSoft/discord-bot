dbd2c0c930febdfed6d3d29c6e9299eff832ad29
Jose Tovar <jtovar26455462@gmail.com>
2024-11-19 16:54:25 -0400

    feat: unify deploy commands logic

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -3,49 +3,53 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { DISCORD_CONFIG } = require('./config');
 
-const commands = [];
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-const token = DISCORD_CONFIG.discordToken;
-const clientId = DISCORD_CONFIG.discordClientId;
-const guildId = DISCORD_CONFIG.discordGuildId;
+module.exports = (client) => {
+  const commands = [];
+  const foldersPath = path.join(__dirname, 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
+  const token = DISCORD_CONFIG.discordToken;
+  const clientId = DISCORD_CONFIG.discordClientId;
+  const guildId = DISCORD_CONFIG.discordGuildId;
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const command = require(path.join(commandsPath, file));
-    if ('data' in command && 'execute' in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+      const command = require(path.join(commandsPath, file));
+      if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
+      }
     }
   }
+
+  const rest = new REST().setToken(token);
+
+  const init = async () => {
+    try {
+      console.log(
+        `Started refreshing ${commands.length} application (/) commands.`
+      );
+
+      const data = await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+      );
+
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands.`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  init();
 }
-
-const rest = new REST().setToken(token);
-
-const init = async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
-
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
-
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-init();

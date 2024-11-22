399103abe4a46a93c0e53b1be92dfcf0ed4ec4ad
Yoel Ferreyra <yoelferreyra24@gmail.com>
2024-11-22 18:47:02 -0300

    feat: change custom message

--- src/commands/utility/requestPoint.js
+++ src/commands/utility/requestPoint.js
@@ -1,17 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { DISCORD_CONFIG } = require('../../config');
 
-const discordQaRoleId =
-    DISCORD_CONFIG.discordQARoleId || '1203085046769262592';
+const adminPointRequestChannel =
+    DISCORD_CONFIG.discordAdminPointRequestChannel || '1309628262456098878';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('request-point')
-        .setDescription('Request point in this theads')
+        .setDescription('Request a point in this thread')
         .addUserOption((option) =>
             option
                 .setName('user')
                 .setDescription('User to search for')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('reason')
+                .setDescription('Reason for requesting the point')
                 .setRequired(true)
         ),
     async execute(interaction) {
@@ -24,21 +30,31 @@ module.exports = {
                     ephemeral: true,
                 });
             }
-            const user = options.getUser('user') || interaction.user;
+
+            const user = options.getUser('user');
+            const userMessage = options.getString('reason');
             const escapedUserId = `<@${user.id}>`;
-            const escapedQaId = `<@&${discordQaRoleId}>`
 
             const threadLink = `https://discord.com/channels/${guild.id}/${channel.id}`;
-            const message = `${escapedQaId} ${escapedUserId} está pidiendo ayuda en este canal: ${threadLink}`;
+            const message = `${escapedUserId} está pidiendo un punto por **${userMessage}**. Revisa el mensaje acá: ${threadLink}`;
 
-            const channelSend = await interaction.client.channels.fetch('1270770663048613983');
+            const channelSend = await interaction.client.channels.fetch(adminPointRequestChannel);
 
             if (channelSend) {
                 await channelSend.send(message);
             }
+
+            await interaction.reply({
+                content: 'Your request has been sent successfully.',
+                ephemeral: true,
+            });
         } catch (error) {
-            console.error(error);
-            await interaction.reply('An error occurred while request QA.');
+            console.error('Error executing request-point command:', error);
+
+            await interaction.reply({
+                content: 'An error occurred while submitting your request. Please try again later.',
+                ephemeral: true,
+            });
         }
     },
 };

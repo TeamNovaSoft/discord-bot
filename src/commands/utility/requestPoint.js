38a2621a3f69ec580d36e55d9ea1f5271afc97ce
Yoel Ferreyra <yoelferreyra24@gmail.com>
2024-11-22 17:47:53 -0300

    feat: adding new command to bot

--- src/commands/utility/requestPoint.js
+++ src/commands/utility/requestPoint.js
@@ -1,40 +1,44 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { MAPPED_STATUS_COMMANDS } = require('../../config');
+const { DISCORD_CONFIG } = require('../../config');
 
-const COMMAND_KEYS = Object.keys(MAPPED_STATUS_COMMANDS);
+const discordQaRoleId =
+    DISCORD_CONFIG.discordQARoleId || '1203085046769262592';
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('request-point')
-    .setDescription('Request point in this theads')
-    .addUserOption((option) =>
-        option
-          .setName('user')
-          .setDescription('User to search for')
-          .setRequired(true)
-    ),
-  async execute(interaction) {
-    try {
-      const { options, channel, guild } = interaction;
+    data: new SlashCommandBuilder()
+        .setName('request-point')
+        .setDescription('Request point in this theads')
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('User to search for')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        try {
+            const { options, channel, guild } = interaction;
 
-      if (!channel.isThread()) {
-        return await interaction.reply({
-          content: 'Sorry, this is not a thread!',
-          ephemeral: true,
-        });
-      }
-      const user = options.getUser('user') || interaction.user;
-      const escapedUserId = `<@${user.id}>`;
+            if (!channel.isThread()) {
+                return await interaction.reply({
+                    content: 'Sorry, this is not a thread!',
+                    ephemeral: true,
+                });
+            }
+            const user = options.getUser('user') || interaction.user;
+            const escapedUserId = `<@${user.id}>`;
+            const escapedQaId = `<@&${discordQaRoleId}>`
 
-      // Generate the message and the thread link
-      const threadLink = `https://discord.com/channels/${guild.id}/${channel.id}`;
-      const message = `@QA ${escapedUserId} está pidiendo ayuda en este canal: ${threadLink}`;
+            const threadLink = `https://discord.com/channels/${guild.id}/${channel.id}`;
+            const message = `${escapedQaId} ${escapedUserId} está pidiendo ayuda en este canal: ${threadLink}`;
 
-      // Send the message to the thread
-      await channel.send(message);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply('An error occurred while request QA.');
-    }
-  },
+            const channelSend = await interaction.client.channels.fetch('1270770663048613983');
+
+            if (channelSend) {
+                await channelSend.send(message);
+            }
+        } catch (error) {
+            console.error(error);
+            await interaction.reply('An error occurred while request QA.');
+        }
+    },
 };

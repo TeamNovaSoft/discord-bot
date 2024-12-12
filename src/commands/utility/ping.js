0bc3f7e033bef106c51580e8070a9747062dfef4
Yoel Ferreyra <yoelferreyra24@gmail.com>
2024-12-12 15:07:28 -0300

    refactor: change with environment

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { translateLanguage } = require('../../languages');
+const { botLanguage } = require('../../config');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription(translateLanguage('es', 'pingCommand.description')),
+    .setDescription(translateLanguage(botLanguage, 'pingCommand.description')),
   async execute(interaction) {
     await interaction.reply('Pong!');
   },
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const {
   scheduleCalendarNotifications,
 } = require('./cron/schedule-google-calendar');
 const { firebaseConfig } = require('../firebase-config');
-const { translateLanguage } = require('./languages');
 
 const client = new Client({
   intents: [
@@ -30,7 +29,5 @@ scheduleMessages(client, cronTimes.messageTimes);
 if (firebaseConfig.scheduledCalendarEnabled) {
   scheduleCalendarNotifications(client);
 }
-
-console.log(translateLanguage('en', 'button.save'));
 
 client.login(token);

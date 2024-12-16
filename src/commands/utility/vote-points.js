8007cb990e636821002442049cd41f1141aa8e9f
Alberto Rey <ajrey1282@gmail.com>
2024-12-16 09:26:58 -0500

    feat: iss-50-add-options-for-point-types

--- src/commands/utility/vote-points.js
+++ src/commands/utility/vote-points.js
@@ -5,23 +5,39 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('vote-points')
     .setDescription(
-      'Start a Effort Estimation Points votation for current Thread'
+      'Start an Effort Estimation Points votation for the current Thread'
     )
     .addUserOption((option) =>
       option
         .setName('user')
         .setDescription('User to search for')
         .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('point-type')
+        .setDescription('Select the type of points to award')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Normal Points', value: 'normal' },
+          { name: 'Boosted Points', value: 'boosted' }
+        )
     ),
   async execute(interaction) {
     const user = interaction.options.getUser('user') || interaction.user;
+    const pointType = interaction.options.getString('point-type');
 
-    await interaction.reply(`<@&${tagIds.taskCompletedTagId}> <@${user.id}>`);
+    const selectedTagId =
+      pointType === 'boosted' ? tagIds.boostedPointTagId : tagIds.addPointTagId;
+
+    await interaction.reply(
+      `<@&${tagIds.taskCompletedTagId}> <@${user.id}> (Awarding: ${pointType.toUpperCase()} Points with Tag <@&${selectedTagId}>)`
+    );
 
     interaction.followUp({
       poll: {
         question: {
-          text: `How much cost this task? | ${user.id}`,
+          text: `How much does this task cost? | ${user.id}`,
           emoji: { name: '🧮' },
         },
         allowMultiselect: false,
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -51,4 +51,4 @@ module.exports = (client) => {
   };
 
   init();
-}
+};

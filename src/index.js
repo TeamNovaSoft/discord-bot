c38caef4bb9dfe9c095b83067573bbaac2e2a5b4
Jose Tovar <jtovar26455462@gmail.com>
2025-02-04 10:30:00 -0400

    feat: add intent to get permissions

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
 });
 client.commands = new Collection();

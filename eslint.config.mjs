0f9d8356d4032c5a97fe8190bf4de87f8d54a3a0
Yoel Ferreyra <yoelferreyra24@gmail.com>
2025-02-19 13:06:19 -0300

    feat: adding new rules and plugins to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import prettierPlugin from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 const { node } = globals;
-
 export default [
   pluginJs.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   {
     files: ['**/*.js'],
     plugins: {
@@ -28,6 +29,10 @@ export default [
       'no-undef': 'error',
       'no-throw-literal': 'error',
       'prefer-promise-reject-errors': 'error',
+      'max-lines': ["error", 250],
+      'max-lines-per-function': ["error", 50],
+      'import/no-dynamic-require': 'warn',
+      'import/no-nodejs-modules': 'warn',
 
       eqeqeq: ['error', 'always'],
       curly: ['error', 'all'],

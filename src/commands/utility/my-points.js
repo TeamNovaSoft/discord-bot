b45fcb6ca2c1120dbe4e960630bc2eef9c9a2b2a
Alberto Rey <ajrey1282@gmail.com>
2025-02-25 14:23:29 -0500

    fix: star and date end

--- src/commands/utility/my-points.js
+++ src/commands/utility/my-points.js
@@ -50,7 +50,7 @@ module.exports = {
         ? channelsInput.split(',').map((channel) => channel.trim())
         : [];
 
-      const targetStartDate = new Date(year, month - 1, 1);
+      const targetStartDate = new Date(year, month - 1, 0);
       const targetEndDate = new Date(year, month, 0);
 
       const fetchedPoints = {
